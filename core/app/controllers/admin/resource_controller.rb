b60570c1f8958990dce0527843e08d3585af6f2c
Roman Smirnov <roman@railsdog.com>
2011-02-25 16:08:27 +0300

    Added support of most used action callbacks, with preserving r_c syntax

--- core/app/controllers/admin/resource_controller.rb
+++ core/app/controllers/admin/resource_controller.rb
@@ -1,10 +1,13 @@
+require 'spree_core/action_callbacks'
 class Admin::ResourceController < Admin::BaseController
   helper_method :new_object_url, :edit_object_url, :object_url, :collection_url
   load_and_authorize_resource
   respond_to :html
-  
+    
   def update
+    invoke_callbacks(:update, :before)
     if object.update_attributes(params[object_name])
+      invoke_callbacks(:update, :after)
       flash[:notice] = I18n.t(:successfully_updated, :scope => object_name)
       respond_with(object, :location => collection_url)
     else
@@ -13,7 +16,9 @@ class Admin::ResourceController < Admin::BaseController
   end
 
   def create
+    invoke_callbacks(:create, :before)
     if object.save
+      invoke_callbacks(:create, :after)
       flash[:notice] = I18n.t(:successfully_created, :scope => object_name)
       respond_with(object, :location => collection_url)
     else
@@ -22,16 +27,44 @@ class Admin::ResourceController < Admin::BaseController
   end
   
   def destroy
+    invoke_callbacks(:destroy, :before)
     if object.destroy
+      invoke_callbacks(:destroy, :after)
       flash[:notice] = I18n.t(:successfully_removed, :scope => object_name)
+      respond_to do |format|
+        format.html { redirect_to collection_url }
+        format.js   { render_js_for_destroy }
+      end
+    else
+      redirect_to collection_url
     end
-    respond_to do |format|
-      format.html { redirect_to collection_url }
-      format.js   { render_js_for_destroy }
-    end
+
   end
  
   protected
+  
+  def self.create
+    @@callbacks ||= {}
+    @@callbacks[:create] ||= Spree::ActionCallbacks.new
+  end
+  
+  def self.update
+    @@callbacks ||= {}
+    @@callbacks[:update] ||= Spree::ActionCallbacks.new
+  end
+  
+  def self.destroy
+    @@callbacks ||= {}
+    @@callbacks[:destroy] ||= Spree::ActionCallbacks.new
+  end
+
+  def invoke_callbacks(action, callback_type)
+    @@callbacks[action] ||= Spree::ActionCallbacks.new
+    case callback_type.to_sym
+      when :before then @@callbacks[action].before_methods.each {|method| send method }
+      when :after  then @@callbacks[action].after_methods.each  {|method| send method }
+    end
+  end
 
   def render_js_for_destroy
     render :partial => "/admin/shared/destroy"
@@ -47,13 +80,14 @@ class Admin::ResourceController < Admin::BaseController
   end
   
   def collection
-    @collection = model_class.all
-    instance_variable_set "@#{controller_name}", @collection  
+    @collection ||= instance_variable_get "@#{controller_name}"
   end
   
   def object
     @object ||= instance_variable_get "@#{object_name}"
   end
+
+  # URL helpers
 
   def new_object_url(options = {})
     new_polymorphic_url([:admin, model_class], options)
