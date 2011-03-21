01c62f225ec120de4ef7edbe2df8c314204883c7
Alexander Shuhin <a.medium@gmail.com>
2011-03-21 17:07:45 +0300

    Tracker model deprecation fix

--- core/app/models/tracker.rb
+++ core/app/models/tracker.rb
@@ -1,5 +1,5 @@
 class Tracker < ActiveRecord::Base
   def self.current
-    Tracker.find(:first, :conditions => {:active => true, :environment => ENV['RAILS_ENV']})
+    Tracker.where('active = ? and environment = ?', true, ENV['RAILS_ENV']).first
   end
 end
